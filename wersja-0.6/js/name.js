8215dc774b86e93d19838e655980d4e8465d82c9
Lsk2127 <150465246+Lsk2127@users.noreply.github.com>
2024-03-07 20:43:46 +0100

    Add files via upload

---
+++ wersja-0.6/js/name.js
@@ -0,0 +1,29 @@
+
+function sendName(){
+	let name = document.getElementById("nameInput").value;
+	
+	if(name!="" && name.length<50){
+		setCookie("name", name);
+	}
+	
+	setWelcome();
+}
+
+function setWelcome(){
+	if(cookieExists("name")){
+		document.getElementById("hide").style.display = "none";
+		
+		let name = valueFromCookie("name");
+		if(name==""){
+			document.getElementById("nameField").innerHTML = "Cześć!";
+		} else {
+			document.getElementById("nameField").innerHTML = "Cześć " + valueFromCookie("name") + "!";
+		}
+	} else {
+		document.getElementById("hide").style.display = "block";
+	}
+}
+
+
+setWelcome();
+
